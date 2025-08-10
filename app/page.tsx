1f1255cf3e3cc6ef68b97ada02e1dc2eddde6d9d
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2025-08-10 06:46:06 +0600

    setup invoice generator app using next.js

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [showPreview, setShowPreview] = useState(false);
+  if (showPreview) {
+  }
+
+  return <div></div>;
+}
